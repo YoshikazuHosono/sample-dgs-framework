b25a4b2528fd979e5dac6ba2520325790e08fb3d
YoshikazuHosono <yyhhwork@gmail.com>
2022-09-21 23:18:19 +0900

    tidy up

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,6 +1,5 @@
 package com.example.demo
 
-import com.netflix.dgs.codegen.generated.types.Show
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
